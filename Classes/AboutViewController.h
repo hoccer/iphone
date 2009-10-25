ad1b0d43b2523930e304e9e167e2e99fc892d6a1
Robert Palmer <robert.palmer@artcom.de>
2009-10-25 16:13:34 +0100

    added empty file for about screen

---
+++ Classes/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  Hoccer
+//
+//  Created by Robert Palmer on 25.10.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutViewController : UIViewController {
+
+}
+
+@end
