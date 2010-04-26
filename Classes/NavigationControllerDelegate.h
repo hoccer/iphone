9d2a0e37deb7861bee90a2311e4160c43768e8fb
Robert Palmer <robert.palmer@artcom.de>
2010-04-26 14:06:05 +0200

    reenabled image display and first tableview experiments

---
+++ Classes/NavigationControllerDelegate.h
@@ -0,0 +1,16 @@
+//
+//  NavigationControllerDelegate.h
+//  Hoccer
+//
+//  Created by Robert Palmer on 26.04.10.
+//  Copyright 2010 Art+Com AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NavigationControllerDelegate : NSObject <UINavigationControllerDelegate> {
+
+}
+
+@end
---
+++ Classes/NavigationControllerDelegate.m
@@ -0,0 +1,21 @@
+//
+//  NavigationControllerDelegate.m
+//  Hoccer
+//
+//  Created by Robert Palmer on 26.04.10.
+//  Copyright 2010 Art+Com AG. All rights reserved.
+//
+
+#import "NavigationControllerDelegate.h"
+
+
+@implementation NavigationControllerDelegate
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+	CGRect frame = navigationController.navigationBar.frame;
+	frame.size.height = 53;
+	
+	navigationController.navigationBar.frame = frame;
+}
+
+@end
