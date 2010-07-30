870158f98195f638e40e5c806d230bc64430c59b
Robert Palmer <robert.palmer@artcom.de>
2010-07-30 11:45:05 +0200

    improved animations of status bar

--- Classes/ViewController/StatusViewController.m
+++ Classes/ViewController/StatusViewController.m
@@ -41,11 +41,11 @@
 @synthesize covered;
 
 - (void)viewDidLoad {
-	[self hideViewAnimated];
 	[self hideRecoverySuggestion];
 	self.view.backgroundColor = [UIColor clearColor];
 	showingError = NO;
 	
+	self.view.layer.hidden = YES;	
 }
 
 - (void)dealloc {
@@ -296,34 +296,44 @@
 #pragma mark Showing and Hiding StatusBar
 
 - (void)showViewAnimated {
+	NSLog(@"hidden: %d", self.view.hidden);
 	if (!self.view.hidden || self.covered) {
 		return;
 	}
 
-	NSLog(@"actions: %@", [self.view.layer actions]);
-	self.view.layer.hidden = NO;
+	self.view.hidden = NO;
 	
 	CGFloat currentYPos = self.view.center.y;
+	NSLog(@"current position: %d", currentYPos);
+
 	self.view.center = CGPointMake(self.view.center.x, currentYPos - 100);
 	[UIView beginAnimations:@"slideDown" context:nil];
 	
 	self.view.center = CGPointMake(self.view.center.x, currentYPos);
-	[UIView setAnimationDuration:0.7];
 	[UIView commitAnimations];
 }
 
 - (void)hideViewAnimated {
+	NSLog(@"hide animation");
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
- 	self.view.layer.hidden = YES;
 	
-	CGFloat currentYPos = self.view.center.y;
-	[UIView beginAnimations:@"slideDown" context:nil];
+	CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+	animation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.center.x, self.view.center.y - 100)];
+	animation.fillMode = kCAFillModeBoth;
+	animation.removedOnCompletion = NO;
+	animation.duration = 0.2;
+	animation.delegate = self;
 	
-	self.view.center = CGPointMake(self.view.center.x, currentYPos);
-	[UIView setAnimationDuration:0.7];
-	[UIView commitAnimations];
+	[self.view.layer addAnimation:animation forKey:@"slideUp"];
+	
+	// self.view.center = CGPointMake(self.view.center.x, currentYPos);
 }
 
+- (void) animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+	NSLog(@"ready");
+	self.view.hidden = YES;
+	[self.view.layer removeAllAnimations];
+}
 
 #pragma mark -
 #pragma mark Monitoring Changes
